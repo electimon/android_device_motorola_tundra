319d9b9536f90b1609f6c11beb6fce8b29a861bc
electimon <electimon@gmail.com>
2023-03-13 21:14:31 +0000

    tundra: Update tree for tundra specific configurations
    Change-Id: I91632fceccad58dd72fb26a12bc498941e93783e
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -58,10 +58,6 @@ PRODUCT_PACKAGES += \
 # Init
 $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
 
 # Keylayout
 PRODUCT_COPY_FILES += \
